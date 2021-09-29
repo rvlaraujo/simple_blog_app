99ac10d963c8b8f02df8cf0508e9865eae4b672a
Rafael Viana <rvlaraujo@gmail.com>
2021-09-29 14:53:56 -0300

    Fixbug entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,5 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle exec rails s -b 0.0.0.0
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
