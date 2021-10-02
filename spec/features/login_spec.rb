81afaf2d1a50c42ff987e523b71e2b3070250e71
Rafael Viana <rvlaraujo@gmail.com>
2021-10-02 18:23:26 -0300

    FactoryBot added

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Login user actions' do
   scenario 'Succesfull login' do
-    user = User.create!(email: 'admin@email.com', password: '123456')
+    user = FactoryBot.create(:user)
     login_as(user, scope: :user)
 
     visit root_path
